fae20195eb279ae4f48dd80683a9c066563e1036
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-11-11 11:33:28 +0700

    minuskomen

--- SegtigaBintangSoal2.java
+++ SegtigaBintangSoal2.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 public class SegtigaBintangSoal2{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in); 
+        Scanner input = new Scanner(System.in); 
+
+        System.out.print("Masukkan nilai N (minimal 5): ");
+        int N = input.nextInt();
+
+        if (N < 5) {
+            System.out.println("Nilai N minimal harus 5 atau lebih.");
+        } else {
+            // Loop untuk setiap baris segitiga
+            for (int i = N; i >= 1; i--) {
+                // Loop untuk mencetak bintang pada setiap baris
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                // Pindah ke baris baru setelah mencetak bintang pada baris tersebut
+                System.out.println();
+            }
+        }
     }
 }
