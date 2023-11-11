557389096c48b5de048633e40877fd568ba53478
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-11-11 22:55:27 +0700

    neww

--- SegtigaBintangSoal2.java
+++ SegtigaBintangSoal2.java
@@ -3,20 +3,17 @@ public class SegtigaBintangSoal2{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in); 
 
-        System.out.print("Masukkan nilai N (minimal 5): ");
-        int N = input.nextInt();
+        System.out.print("Masukkan nilai N (minimal 5): "); // Memberi perintah untuk memasukan nilai yang ingin diolah
+        int N = input.nextInt(); // Deklarasi Untuk Nilai Yang Akan Diolah
 
         if (N < 5) {
-            System.out.println("Nilai N minimal harus 5 atau lebih.");
+            System.out.println("Nilai N minimal harus 5 atau lebih."); // Memberi batasan nilai 
         } else {
-            // Loop untuk setiap baris segitiga
-            for (int i = N; i >= 1; i--) {
-                // Loop untuk mencetak bintang pada setiap baris
-                for (int j = 1; j <= i; j++) {
-                    System.out.print("*");
+            for (int i = N; i >= 1; i--) { // Loop untuk setiap baris segitiga
+                for (int j = 1; j <= i; j++) { // Loop untuk mencetak bintang pada setiap baris
+                    System.out.print("*"); // Mencetak bintang
                 }
-                // Pindah ke baris baru setelah mencetak bintang pada baris tersebut
-                System.out.println();
+                System.out.println(); // Perpindahan baris setelah mencetak bintang pada baris sebelumnya
             }
         }
     }
